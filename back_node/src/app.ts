12f0d4b9a71cf8947ac4981d05ee245c8208184a
larissa-sato <larissasato_@outlook.com>
2023-02-10 17:32:48 -0300

    added list user id services, controller and routes

--- back_node/src/app.ts
+++ back_node/src/app.ts
@@ -1,5 +1,5 @@
-import "reflect-metadata";
 import "express-async-errors";
+import "reflect-metadata";
 import express from "express";
 import handleErrorMiddleware from "./middlewares/handleErrorMiddleware.middleware";
 import loginRouter from "./routes/userLoginRouter.routes";
